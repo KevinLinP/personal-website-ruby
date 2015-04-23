bcf379704e1befa063e8fa7a66f1e01fc5c95fba
Kevin Lin <johndoe@example.com>
2015-04-22 22:22:02 -0400

    Fixing resume updated at.

--- resume_app.rb
+++ resume_app.rb
@@ -6,13 +6,11 @@ require 'slim'
 # TODO: Google Analytics
 # TODO: Optimizely?
 
-get '/' do
-  last_resume_commit = `git log --max-count=1 views/resume.slim`
-  last_updated = last_resume_commit.match(/Date:\s+(.+)$/).captures.first
+RESUME_LAST_UPDATED = DateTime.new
 
+get '/' do
   local_vars = {
     serious_print: !!params[:srs],
-    last_updated: DateTime.parse(last_updated),
     phone_number: nil
   }
   local_vars[:phone_number] = ENV['KEVINS_PHONE_NUMBER'] if !!params[:beyonce] # put a ring on it.
