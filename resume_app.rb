92d2007e1304386a6538a22e69a4d654eda854e4
Kevin Lin <johndoe@example.com>
2015-04-22 22:14:40 -0400

    Revamped date capturing.

--- resume_app.rb
+++ resume_app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'slim'
+require 'rugged'
 
 # TODO: rewrite Avvo section
 # TODO: optimize print layout
@@ -7,8 +8,12 @@ require 'slim'
 # TODO: Optimizely?
 
 get '/' do
+  last_resume_commit = `git log --max-count=1 views/resume.slim`
+  last_updated = last_resume_commit.match(/Date:\s+(.+)$/).captures.first
+
   local_vars = {
     serious_print: !!params[:srs],
+    last_updated: DateTime.parse(last_updated),
     phone_number: nil
   }
   local_vars[:phone_number] = ENV['KEVINS_PHONE_NUMBER'] if !!params[:beyonce] # put a ring on it.
