54e37da178795f58cea8846ede1478d3e1ea5886
Kevin Lin <johndoe@example.com>
2015-04-22 20:51:47 -0400

    Added beyonce.

--- resume_app.rb
+++ resume_app.rb
@@ -2,11 +2,16 @@ require 'sinatra'
 require 'slim'
 
 # TODO: rewrite Avvo section
-# TODO: beyonce
 # TODO: optimize print layout
 # TODO: Google Analytics
 # TODO: Optimizely?
 
 get '/' do
-  slim :resume, locals: {serious_print: !!params[:srs]}
+  local_vars = {
+    serious_print: !!params[:srs],
+    phone_number: nil
+  }
+  local_vars[:phone_number] = ENV['KEVINS_PHONE_NUMBER'] if !!params[:beyonce] # put a ring on it.
+
+  slim :resume, locals: local_vars
 end
