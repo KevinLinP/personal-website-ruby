aeda857f81b56719cf53beca6c37d203e66cde93
Kevin Lin <johndoe@example.com>
2015-05-05 15:03:32 -0700

    Added many phone contact links.

--- resume_app.rb
+++ resume_app.rb
@@ -1,15 +1,14 @@
 require 'sinatra'
 require 'slim'
 
-# TODO: rewrite Avvo section
 # TODO: Optimizely?
 
 get '/' do
   local_vars = {
+    show_phone_number: !!params[:ring],
     serious_print: !!params[:srs],
-    phone_number: nil
+    phone_number: ENV['KEVINS_PHONE_NUMBER']
   }
-  local_vars[:phone_number] = ENV['KEVINS_PHONE_NUMBER'] if !!params[:beyonce] # put a ring on it.
 
   slim :resume, locals: local_vars
 end
