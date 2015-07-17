afe67dc67b34da3d56f5803c3aaa8d04d29acb2f
Kevin Lin <kevin.lin.p@gmail.com>
2015-07-17 00:49:36 -0700

    Added new route. Updated resume. Added status signup link.

--- personal_website.rb
+++ personal_website.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 require 'slim'
 
-# TODO: Optimizely?
-
 get '/' do
+  redirect to('/resume'), 302
+end
+
+get '/resume' do
   local_vars = {
     show_phone_number: !!params[:ring], # put a ring on it!
     serious_print: !!params[:srs],
